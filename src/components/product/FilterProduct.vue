dc19966ad2a50b51e740c3e92d6180d299b1a858
Murod-beck <murodbeck.off@gmail.com>
2022-11-01 23:04:24 +0300

    sort

--- src/components/product/FilterProduct.vue
+++ src/components/product/FilterProduct.vue
@@ -1,47 +1,56 @@
 <template>
   <h6 class="color">Saralash</h6>
-  <select ref="select" v-model="producta">
+  <select class="browser-default">
     <option value="" disabled selected>Yangilari</option>
-    <option>{{ producta }}</option>
+    <option value="minPrice">Arzondan</option>
+    <option value="maxPrice">Qimmatidan</option>
   </select>
   <h6 class="color">Ko'rsatish</h6>
-  <select ref="selects">
+  <select class="browser-default">
     <option value="" disabled selected>10</option>
     <option value="2">20</option>
     <option value="3">30</option>
   </select>
+  <table>
+    <tr>
+      <td><a class="btn" @click="sortParam = 'title'">Title</a></td>
+      <td><a class="btn" @click="sortParam = 'price'">Price</a></td>
+    </tr>
+    <tr v-for="phone in sortedList" :key="phone">
+      <td>{{ phone.title }}</td>
+      <td>{{ phone.price }}</td>
+    </tr>
+  </table>
 </template>
 
 <script>
 export default {
   name: 'Filters',
-  created() {},
   data() {
     return {
       select: null,
-      producta: '',
     };
   },
   props: ['products'],
-  methods: {
-    sortAZ() {
-      console.log(filterData());
-    },
-  },
-  computed: {
-    filterData() {
-      let comp = this.producta;
-      return this.products.filter(function (elem) {
-        if (comp === '') return true;
-        else return elem.producta.indexOf(comp) > -1;
-      });
-    },
-  },
-  mounted() {
-    M.FormSelect.init(this.$refs.selec);
-  },
-  props: {},
   methods: {},
+  computed: {
+    sortedList() {
+      let sortTitle = function (d1, d2) {
+        return d1.title.toLowerCase() > d2.title.toLowerCase() ? 1 : -1;
+      };
+      let sortPrice = function (d1, d2) {
+        return d1.price > d2.price ? 1 : -1;
+      };
+      switch (this.sortParam) {
+        case 'title':
+          return this.products.sort(sortTitle);
+        case 'price':
+          return this.products.sort(sortPrice);
+        default:
+          return this.products;
+      }
+    },
+  },
 };
 </script>
 
